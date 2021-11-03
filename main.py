eefbf30ac69f9aad0c3d35f64eb7dfc00fe5f88e
DawidDGT <66462796+DawidDGT@users.noreply.github.com>
2021-11-03 21:34:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def sandra(ctx):
 
 
 @client.command()
-async def v(ctx,kod):
+async def v(ctx, kod):
     channel = (828011225292079124)
     global hasloo
     if ctx.channel.id == channel:
@@ -42,10 +42,12 @@ async def v(ctx,kod):
             embed1.set_thumbnail(url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/White_check_mark_in_dark_green_rounded_square.svg/1024px-White_check_mark_in_dark_green_rounded_square.svg.png")
             embed1.set_footer(text = f"{ctx.author}", icon_url = ctx.author.avatar_url)
             verifyrole = (discord.utils.get(ctx.guild.roles, name = "weryfikacja"))
+            verifyrole2 = (discord.utils.get(ctx.guild.roles, name = "weryfikacja"))
             await ctx.author.remove_roles(verifyrole)
+            await ctx.author.add_roles(verifyrole2)
             await ctx.author.send(embed=embed1)
             await ctx.send("You have been verified!")
-            await ctx.channel.purge(limit=1)
+            await ctx.channel.purge(limit=2)
 
 
 
@@ -58,13 +60,13 @@ async def on_member_join(member):
 
     haslo = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    haslo2 = 3
+    haslo2 = 5
 
     hasloo = "".join(random.sample(haslo,haslo2))
 
 
     channel = member.guild.get_channel(828011225292079124)
-    await channel.send(f"{member.mention}**Please type:** `!v {hasloo}`"
+    await channel.send(f"{member.mention}**Please type:** `!v {hasloo}`")
 
 
 @client.event
@@ -74,7 +76,7 @@ async def on_message(message):
     if message.channel.id == channel and message.content and message.author != client.user:
         await message.delete()
         embed1=discord.Embed(title="ERROR", color=0xff0000,timestamp=datetime.utcnow())
-        embed1.add_field(name="\u200b", value=f"**Please use !verify {hasloo}**", inline=False)
+        embed1.add_field(name="\u200b", value=f"**Please use !v {hasloo}**", inline=False)
         embed1.set_thumbnail(url = "https://emoji.gg/assets/emoji/1326_cross.png")
         await message.author.send(embed=embed1)
 
@@ -82,3 +84,9 @@ async def on_message(message):
 
 TOKEN = os.getenv("BOT_TOKEN")
 client.run(TOKEN)
+
+client.run(TOKEN)
+
+
+
+
